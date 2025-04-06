3ade6898f8779947abe302bd5bef1d6a691262d0
leandrocgsi <leandrocgsi@gmail.com>
2025-04-06 02:18:04 -0300

    Implementing the React JS Client for the Second Project with .NET AI, OpenAI and ChatGPT

---
+++ 09_Integrating.NET_AI_OpenAI_OpenAI_VoiceTranscriptionReactJS/api/DotNetAiErudio-VoiceTranscription/DotNetAiErudio-VoiceTranscription/Extensions/OpenAIExtensions.cs
@@ -0,0 +1,22 @@
+﻿using OpenAI;
+
+namespace DotNetAiErudio_VoiceTranscription.Extensions
+{
+    public static class OpenAIExtensions
+    {
+        public static WebApplicationBuilder AddOpenAI(this WebApplicationBuilder builder)
+        {
+            var apiKey = Environment.GetEnvironmentVariable("OPEN_AI_API_KEY");
+
+            if (string.IsNullOrEmpty(apiKey))
+            {
+                throw new InvalidOperationException("OpenAI API key is not set.");
+            }
+
+            var openAIClient = new OpenAIClient(apiKey);
+
+            builder.Services.AddSingleton(openAIClient);
+            return builder;
+        }
+    }
+}
---
+++ 09_Integrating.NET_AI_OpenAI_OpenAI_VoiceTranscriptionReactJS/api/DotNetAiErudio-VoiceTranscription/DotNetAiErudio-VoiceTranscription/Program.cs
@@ -0,0 +1,70 @@
+using DotNetAiErudio_VoiceTranscription.Extensions;
+using DotNetAiErudio_VoiceTranscription.Service;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.AddOpenAI();
+
+builder.Services.AddSingleton<TranscriptionService>();
+
+builder.Services.AddCors(options => options.AddDefaultPolicy(builder =>
+{
+    builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+}));
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddOpenApi(options =>
+{
+    options.AddDocumentTransformer((document, context, _) =>
+    {
+        document.Info = new()
+        {
+            Title = ".NET AI Erudio API",
+            Version = "v1",
+            Description = """  
+               This API provides AI-based features such as chat, image generation,  
+               recipe creation and audio transcription.  
+               """,
+            Contact = new()
+            {
+                Name = "Erudio Training",
+                Email = "contact@erudio.com.br",
+                Url = new Uri("https://pub.erudio.com.br/meus-cursos")
+            },
+            License = new()
+            {
+                Name = "Apache 2 License",
+                Url = new Uri("https://pub.erudio.com.br/meus-cursos")
+            },
+            TermsOfService = new Uri("https://pub.erudio.com.br/meus-cursos")
+        };
+        return Task.CompletedTask;
+    });
+});
+
+var app = builder.Build();
+
+app.UseCors();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference(options =>
+    {
+        options.Title = ".NET AI Erudio API";
+        options.Theme = ScalarTheme.Default;
+        options.DefaultHttpClient = new(ScalarTarget.Http, ScalarClient.Http11);
+    });
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
