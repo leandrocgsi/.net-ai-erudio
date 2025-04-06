3cd4ae5826d3ec2d9a1327a5aa45193522c443b1
leandrocgsi <leandrocgsi@gmail.com>
2025-04-06 10:04:07 -0300

    DeepSeek from Zero to Integration with .NET AI and React JS

---
+++ 10_Integrating.NET_AI_DeepSeek/api/DotNetAiErudio/DotNetAiErudio/Extensions/OpenAIExtensions.cs
@@ -0,0 +1,28 @@
+﻿using OpenAI;
+using System.ClientModel;
+
+namespace DotNetAiErudio.Extensions
+{
+    public static class OpenAIExtensions
+    {
+        public static WebApplicationBuilder AddOpenAI(this WebApplicationBuilder builder)
+        {
+            var apiKey = Environment.GetEnvironmentVariable("DEEP_SEEK_API_KEY");
+
+            if (string.IsNullOrEmpty(apiKey))
+            {
+                throw new InvalidOperationException("OpenAI API key is not set.");
+            }
+
+            var option = new OpenAIClientOptions
+            {
+                Endpoint = new Uri("https://api.deepseek.com"),
+            };
+
+            var openAIClient = new OpenAIClient(new ApiKeyCredential(apiKey), option);
+
+            builder.Services.AddSingleton(openAIClient);
+            return builder;
+        }
+    }
+}
