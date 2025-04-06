cfe5d0075e1b5d091f8c067c73c60b021f14b1b1
leandrocgsi <leandrocgsi@gmail.com>
2025-04-05 23:05:19 -0300

    Implementing the React JS Client to Consume the API with .NET AI, OpenAI and ChatGPT

---
+++ 07_Integrating.NET_AI_OpenAI_OpenAI_ClientReactJS/api/DotNetAiErudio/DotNetAiErudio/Controller/GenerativeAIController.cs
@@ -0,0 +1,76 @@
+﻿using DotNetAiErudio.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetAiErudio.Controller
+{
+    [ApiController]
+    public class GenerativeAIController : ControllerBase
+    {
+        private readonly ChatService _chatService;
+        private readonly RecipeService _recipeService;
+        private readonly ImageService _imageService;
+
+        public GenerativeAIController(
+            ChatService chatService,
+            RecipeService recipeService,
+            ImageService imageService
+        )
+        {
+            _chatService = chatService;
+            _recipeService = recipeService;
+            _imageService = imageService;
+        }
+
+        [HttpGet("ask-ai")]
+        public async Task<IActionResult> AskAi([FromQuery] string prompt)
+        {
+            if (string.IsNullOrWhiteSpace(prompt))
+            {
+                return BadRequest("The 'prompt' parameter is required and cannot be empty.");
+            }
+            var response = await _chatService.GetResponseAsync(prompt);
+            return Ok(response);
+        }
+
+        [HttpGet("ask-ai-options")]
+        public async Task<IActionResult> AskAiWithOptions([FromQuery] string prompt)
+        {
+            if (string.IsNullOrWhiteSpace(prompt))
+            {
+                return BadRequest("The 'prompt' parameter is required and cannot be empty.");
+            }
+            var response = await _chatService.GetResponseWithOptionsAsync(prompt);
+            return Ok(response);
+        }
+
+        [HttpGet("recipe-creator")]
+        public async Task<IActionResult> GenerateRecipe(
+            [FromQuery] string ingredients,
+            [FromQuery] string cuisine = "any",
+            [FromQuery] string dietaryRestrictions = "none")
+        {
+            if (string.IsNullOrWhiteSpace(ingredients))
+            {
+                return BadRequest("The 'ingredients' parameter is required and cannot be empty.");
+            }
+            var recipe = await _recipeService.GenerateRecipe(ingredients, cuisine, dietaryRestrictions);
+            return Ok(recipe);
+        }
+
+        [HttpGet("generate-image")]
+        public async Task<IActionResult> GenerateImage(
+            [FromQuery] string prompt,
+            [FromQuery] string quality = "hd",
+            [FromQuery] int n = 1,
+            [FromQuery] int height = 1024,
+            [FromQuery] int width = 1024)
+        {
+            if (string.IsNullOrWhiteSpace(prompt))
+            {
+                return BadRequest("The 'prompt' parameter is required and cannot be empty.");
+            }
+            var imageUrls = await _imageService.GenerateImage(prompt, quality, n, height, width);
+            return Ok(imageUrls);
+        }
+    }
+}
---
+++ 07_Integrating.NET_AI_OpenAI_OpenAI_ClientReactJS/api/DotNetAiErudio/DotNetAiErudio/Extensions/OpenAIExtensions.cs
@@ -0,0 +1,23 @@
+﻿using OpenAI;
+
+namespace DotNetAiErudio.Extensions
+{
+    public static class OpenAIExtensions
+    {
+        public static WebApplicationBuilder AddOpenAI(this WebApplicationBuilder builder)
+        {
+            // var apiKey = builder.Configuration["OpenAI:Key"];
+            var apiKey = Environment.GetEnvironmentVariable("OPEN_AI_API_KEY");
+
+            if (string.IsNullOrEmpty(apiKey))
+            {
+                throw new InvalidOperationException("OpenAI API key is not set.");
+            }
+
+            var openAIClient = new OpenAIClient(apiKey);
+
+            builder.Services.AddSingleton(openAIClient);
+            return builder;
+        }
+    }
+}
